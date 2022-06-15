2f5f09cfae98e5577e1ed1fda95dca2892bd38e7
21aLeX <aleksandra12mail@mail.ru>
2022-06-15 17:43:02 +0800

    new

--- src/App.js
+++ src/App.js
@@ -2,19 +2,34 @@ import './App.css';
 import React, { useState, useMemo } from 'react';
 import Filter from './components/Filter';
 import Table from './components/Table';
+import GetData from './components/bd';
 
 function App() {
   const [column, setColumn] = useState('')
   const [operation, setOperation] = useState('')
   const [condiition, setCondition] = useState('')
+const bd = GetData.getAll()
 
   const array = [{
-    date:'20/05/2022',
+    date: '20/05/2022',
     title: 'help',
-    amount: 4, 
-    distace:67}]
+    amount: 4,
+    distance: 8,
+  },
+  {
+    date: '06/06/2022',
+    title: 'please',
+    amount: 6,
+    distance: 12,
+  },
+  {
+    date: '20/05/2022',
+    title: 'me',
+    amount: 2,
+    distance: 4,
+  }]
 
-    let arr = array
+  let arr = array
 
   const setIf = (column, operation, condiition) => {
     setColumn(column)
@@ -24,20 +39,33 @@ function App() {
   }
 
   useMemo(() => {
-    if (column !==  '' & operation !== ''  & condiition !== '') {
-      if((column === 'amount' || column === 'distance') && !isNaN(condiition)){
-       
-      arr = array.filter(a => 
-       { if(a[column]+operation+condiition) return true}
-         )
-         console.log(arr)
+    if (column !== '' & operation !== '' & condiition !== '') {
+      if(operation == 'включает'){
+        arr = array.filter(a => 
+         String (a[column]).toLowerCase().includes(condiition)
+        )
       }
+     else if ((column === 'amount' || column === 'distance') && !isNaN(condiition)) {
+        arr = array.filter(a => {
+          switch (operation) {
+            case '===':
+              return a[column] == condiition
+            case '>':
+              return a[column] > condiition
+            case '<':
+              return a[column] < condiition
+          }
+        }
+        )
+      }
+
     }
+
   }, [column, operation, condiition])
   return (
     <div className="App">
       <Filter setIf={setIf} />
-      <Table array = {arr} />
+      <Table array={arr} />
     </div>
   );
 }
---
+++ src/components/bd.js
@@ -0,0 +1,14 @@
+// import axios from 'axios'
+
+export default class GetData {
+    static async getAll() {
+      const Pool = require('pg').Pool
+      const pool = new Pool({
+        user: 'my_user',
+        host: 'localhost',
+        database: 'my_database',
+        password: 'root',
+        port: 5432,
+      });
+      }
+}
